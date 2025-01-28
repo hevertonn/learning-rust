1362905277011af6ed6503d57a608f85c7c9c928
Heverton <hb.hevertonborges@gmail.com>
2025-01-28 17:26:10 -0300

    structs

---
+++ structs/src/main.rs
@@ -0,0 +1,39 @@
+#[derive(Debug)]
+struct User {
+    active: bool,
+    name: String,
+    age: u8,
+}
+
+fn main() {
+    let mut user1 = User {
+        active: true,
+        name: String::from("David"),
+        age: 80,
+    };
+
+    let user2 = User {
+        active: false,
+        ..user1
+    };
+
+    user1.name = String::from("Elric");
+
+    let user3 = create_user(String::from("Alfonso"), 50);
+
+    println!("{}", user3.name);
+
+    println!("{}", user2.name);
+
+    println!("{}", user1.name);
+
+    println!("{:#?}", user1);
+}
+
+fn create_user(name: String, age: u8) -> User {
+    User {
+        active: true,
+        name,
+        age,
+    }
+}
